e134810490e1427edac3afc1aa539ecdbc4b1d9f
Uzair <mtu880@gmail.com>
2026-02-18 00:15:01 +0500

    navbar for mobile updated

--- app/components/SideNav.tsx
+++ app/components/SideNav.tsx
@@ -21,9 +21,9 @@ export default function SideNav({
   cartCount = 2,
 }: SideNavProps) {
   return (
-    <nav className="fixed left-4 lg:left-6 top-1/2 -translate-y-1/2 z-50">
-      <div className="glass flex flex-col items-center gap-8 py-10 px-4 rounded-full">
-        <div className="flex flex-col items-center gap-6">
+    <nav className="fixed bottom-6 left-8 right-8 z-50 lg:bottom-auto lg:left-6 lg:right-auto lg:top-1/2 lg:-translate-y-1/2">
+      <div className="glass-nav flex flex-row items-center justify-around gap-2 py-3 px-4 rounded-full shadow-lg lg:flex-col lg:items-center lg:gap-8 lg:py-10 lg:px-4 lg:pb-10 max-w-[280px] mx-auto lg:max-w-none lg:mx-0">
+        <div className="flex flex-row items-center justify-around gap-4 flex-1 lg:flex-col lg:flex-initial lg:gap-6">
           {items.map((item) => (
             <a
               key={item.icon}
@@ -38,14 +38,14 @@ export default function SideNav({
               >
                 {item.icon}
               </span>
-              <span className="absolute left-14 bg-background-dark px-3 py-1 rounded text-xs opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none whitespace-nowrap">
+              <span className="absolute left-1/2 -translate-x-1/2 bottom-full mb-2 bg-background-dark px-3 py-1 rounded text-xs opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none whitespace-nowrap lg:left-14 lg:bottom-auto lg:mb-0 lg:translate-x-0">
                 {item.label}
               </span>
             </a>
           ))}
 
           {/* Divider */}
-          <div className="w-8 h-px bg-white/10" />
+          <div className="w-px h-8 bg-white/10 lg:w-8 lg:h-px" />
 
           {/* Cart */}
           <a
@@ -58,7 +58,7 @@ export default function SideNav({
             >
               shopping_bag
             </span>
-            <span className="absolute left-14 bg-background-dark px-3 py-1 rounded text-xs opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none whitespace-nowrap">
+            <span className="absolute left-1/2 -translate-x-1/2 bottom-full mb-2 bg-background-dark px-3 py-1 rounded text-xs opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none whitespace-nowrap lg:left-14 lg:bottom-auto lg:mb-0 lg:translate-x-0">
               Cart ({cartCount})
             </span>
           </a>
